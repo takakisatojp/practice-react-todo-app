70d2ac4fbff40f7c6113013fab29aa2029abcd57
Takaki Sato <takakisatojp@TakakinoMacBook-Pro.local>
2024-07-11 18:57:31 +0900

    initial commit

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,12 @@
+export const ColorfulMessage = (props) => {
+    const contentStyle = {
+        color: props.color ,
+        fontSize: "18px" ,
+    };
+
+    return (
+        <p style={contentStyle}>{props.children}</p>
+    )
+
+
+}
