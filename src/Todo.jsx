ae71fe8c798ea005afea3c582359e9d4322f8e7f
Takaki Sato <takakisatojp@TakakinoMacBook-Pro.local>
2024-07-12 12:24:43 +0900

    タスクの削除機能

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -21,6 +21,12 @@ export const Todo = () => {
       setIncompleteTodos(newTodos);
     };
 
+    const onClickDelete = (index) => {
+      const newTodos = [...incompleteTodos];
+      newTodos.splice(index,1);
+      setIncompleteTodos(newTodos);
+    };
+
 
 
   return (
@@ -34,12 +40,12 @@ export const Todo = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo,index) => (
               <li key={todo}>
                 <div className="list-low">
                   <p className="todo-item">{todo}</p>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               </li>
             )
