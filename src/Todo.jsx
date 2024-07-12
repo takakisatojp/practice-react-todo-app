ae132de36e0c126bf08c9b07c46232f7e0b79acd
Takaki Sato <takakisatojp@TakakinoMacBook-Pro.local>
2024-07-12 12:50:24 +0900

    タスクの完了機能

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,7 +8,7 @@ export const Todo = () => {
      "Todoです2"
     ]);
 
-    const [completeTodos, setcompleteTodos] = useState([
+    const [completeTodos, setCompleteTodos] = useState([
       "Todoでした1",
        "Todoでした2"
       ]);
@@ -25,6 +25,16 @@ export const Todo = () => {
       const newTodos = [...incompleteTodos];
       newTodos.splice(index,1);
       setIncompleteTodos(newTodos);
+    };
+
+    const onClickComplete = (index) => {
+       const newIncompleteTodos = [...incompleteTodos];
+       newIncompleteTodos.splice(index, 1);
+
+       const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+       setIncompleteTodos(newIncompleteTodos);
+       setCompleteTodos(newCompleteTodos);
+
     };
 
 
@@ -44,7 +54,7 @@ export const Todo = () => {
               <li key={todo}>
                 <div className="list-low">
                   <p className="todo-item">{todo}</p>
-                  <button>完了</button>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
                   <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               </li>
